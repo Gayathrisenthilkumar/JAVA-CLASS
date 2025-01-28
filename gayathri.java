1fb70bc0416128f8b56962d402ff47cb3b81e5c8
Gayathrisenthilkumar <164990837+Gayathrisenthilkumar@users.noreply.github.com>
2025-01-27 22:02:25 -0800

    Add files via upload

---
+++ gayathri.java
@@ -0,0 +1,10 @@
+public class gayathri {
+  public static void main(String[] args)
+{
+	int container =450;
+	String kumar="k";
+	String container1=kumar;
+	System.out.println(container);
+	System.out.println(container1);
+}
+}
