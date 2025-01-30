3ca83751b05411ce6439fb9115988e1761ce7fb7
Gayathrisenthilkumar <164990837+Gayathrisenthilkumar@users.noreply.github.com>
2025-01-29 22:33:22 -0800

    Add files via upload

---
+++ ifelsefn.java
@@ -0,0 +1,14 @@
+public class ifelsefn {
+    public static void main(String[] args)
+    {
+        int num1 = 40;
+        int num2 = 60;
+        if(num1>num2) {
+            System.out.println("Num1 is Greater");
+        }
+        else{
+            System.out.println("Num2 is Greator");
+        
+        }
+    }
+}
