3aa1c27d7b2ffa38f578e73593b56a9c9aa05072
Gayathrisenthilkumar <164990837+Gayathrisenthilkumar@users.noreply.github.com>
2025-01-30 01:02:43 -0800

    Add files via upload

---
+++ TDArray.java
@@ -0,0 +1,20 @@
+public class TDArray{
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1,2,3,4},
+            {5,6,7,8}
+            {9,10,11,12}
+        };
+        System.out.println("Element at row 1,column 2:"+matrix[0][1]);
+        System.out.println("Element at row 3,column 4:" +matrix[2][3]);
+        System.out.println("Printing all elements in the 2D array:");
+        for (int i=0; i<matrix.length; i++) {
+            for (int j=0; j<matrix[i].length; j++);{
+                System.out.println(matrix[i][j] = " ")
+               }
+               System.out.println();
+               
+
+        }
+    }
+}
