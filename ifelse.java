e39904e28a014b4599dc5d4b0b154fcc439a669b
Gayathrisenthilkumar <164990837+Gayathrisenthilkumar@users.noreply.github.com>
2025-01-29 22:30:55 -0800

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,12 @@
+public class ifelse {
+    public static void main(String[] args)
+    {
+        boolean rain= false;
+        if(rain){
+            System.out.println("Take an Umbrella");
+        else{
+            System.out.prinln("Enjoy the Sunlight")
+        }
+        }
+    }
+}
